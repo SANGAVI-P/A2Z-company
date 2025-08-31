dc23568fa8ae82df34d56aa5e5b838e42d8c316a
[dyad] <git@dyad.sh>
2025-08-31 19:58:00 +0530

    [dyad] Added gradient background - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -10,7 +10,7 @@ import { Footer } from "@/components/Footer";
 
 const Index = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-slate-900 text-white">
+    <div className="flex flex-col min-h-screen bg-gradient-to-br from-indigo-900 via-purple-900 to-pink-900 text-white">
       <Header />
       <main className="flex-1">
         <Hero />
